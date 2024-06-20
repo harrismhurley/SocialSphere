c06b2a2850e0f8b79b728ae2dc54f2400682989a
harrismhurley <harrismhurley@icloud.com>
2024-06-20 15:48:40 -0500

    setup server and connections

---
+++ controllers/home-routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const { Post, User } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ["username", "id", "profile_pic"],
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+      current_user_id: req.session.user_id,
+    });
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
