7cd455b543efeeb8d8c6347989a7e38be375156f
harrismhurley <harrismhurley@icloud.com>
2024-06-25 18:26:16 -0500

    connected to postgres server

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,20 @@ require("dotenv").config();
 
 const Sequelize = require("sequelize");
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME,
-      process.env.DB_USER,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST || "localhost",
-        dialect: "postgres",
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+let sequelize;
+
+if (process.env.DB_URL) {
+  sequelize = new Sequelize(process.env.DB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+  );
+}
 
 module.exports = sequelize;
